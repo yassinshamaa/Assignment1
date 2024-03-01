8c7771cc0a028cc65c75b3c02be2732c682c8014
alimontasser <159151995+alimontasser@users.noreply.github.com>
2024-03-01 16:58:37 +0200

    Add files via upload

---
+++ loginwindow.h
@@ -0,0 +1,26 @@
+#ifndef LOGINWINDOW_H
+#define LOGINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class LoginWindow;
+}
+QT_END_NAMESPACE
+
+class LoginWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    LoginWindow(QWidget *parent = nullptr);
+    ~LoginWindow();
+
+private slots:
+    void on_pushButton_2_Register_clicked();
+
+private:
+    Ui::LoginWindow *ui;
+};
+#endif // LOGINWINDOW_H
